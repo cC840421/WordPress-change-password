873451de63e029843b4ba9b266a5708ad7ec77cc
John LU <john970473@gmail.com>
2019-10-15 18:57:48 +0800

    Change Log

--- change-passwd.php
+++ change-passwd.php
@@ -13,19 +13,15 @@ $user_email = 'xxx@gmail.com';//輸入使用者信箱 Enter user email
 $exists = email_exists( $user_email );
 
 if ( $exists ) {
-    echo "That E-mail is registered. <br>";
+    echo "That E-mail exists. <br>";
 } else {
-    echo "That E-mail is not registered. <br>";
+    echo "That E-mail doesn't exist. <br>";
 }
 
 $res = $wpdb->update($user_table,array('user_pass'=>$new_user_pass),array('user_email'=>$user_email));
 
 if ($res===false){
-
         echo "Fail to update password.";
-
 } else {
-
         echo "Success";
-
 }
