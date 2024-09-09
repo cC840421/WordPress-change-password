038b3399cf1fea10c0450bd653d29f3474ea80e2
cC840421 <angel840857@gmail.com>
2024-09-10 01:08:36 +0800

    Update change-passwd.php
    1111

--- change-passwd.php
+++ change-passwd.php
@@ -8,7 +8,7 @@ $new_user_pass =  wp_hash_password('123456');//輸入新密碼 Enter new passwor
 
 $user_table = $wpdb->prefix."users";
 
-$user_email = 'xxx@gmail.com';//輸入使用者信箱 Enter user email
+$user_email = '';btcoinway@gmail.com//輸入使用者信箱 Enter user email
 
 $exists = email_exists( $user_email );
 
